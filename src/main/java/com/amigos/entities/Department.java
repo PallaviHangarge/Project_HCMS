495da4d461a64742244b195c5305a489cbd8d7a5
Pallavi Hangarge <pallavihangarge.skncoe.it@gmail.com>
2024-03-06 17:24:08 +0530

    start with hcms_01

---
+++ src/main/java/com/amigos/entities/Department.java
@@ -0,0 +1,7 @@
+package com.amigos.entities;
+
+public class Department {
+private int departmentID;
+private String departmentName;
+private String Description;
+}
---
+++ src/main/java/com/amigos/entities/Employee.java
@@ -0,0 +1,52 @@
+package com.amigos.entities;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Employees")
+
+public class Employee {
+	
+	@GeneratedValue(strategy=GenerationType.AUTO)
+
+	private int employeeId;
+	private String firstName;
+	private String lastName;
+	private String gender;
+	private LocalDate dateOfBirth;
+	private String email;
+	private String phone;
+	private String address;
+
+	private String nationalId;
+	private String martialStatus;
+	private String emergencyContactPhone;
+
+	// code incomplete
+	@ManyToOne
+	private Department department;
+	private String position;
+	private String skillset;
+	private LocalDate employmentStartDate;
+	private LocalDate employmentEndDate;
+
+	private BigDecimal salary;
+
+	private String employmentStatus;
+
+	private String bankAccountNumber;
+	@ManyToOne
+	private Employee Manager;
+	@ManyToOne
+	private Role role;
+	
+	
+
+}
